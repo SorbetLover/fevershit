bd225fd1dc86bdfd788f3eb3a2e0819d06a7b51e
umc4r4aa3 <umc4r4aa3@gmail.com>
2025-01-07 00:29:49 -0300

    half of menu

--- data/states/FMenu.hx
+++ data/states/FMenu.hx
@@ -1,24 +1,104 @@
-var tunnelBG:FlxSprite;
-var train:FlxSprite;
-var tunnelBG2:FlxSprite;
+var tunnelBG:FunkinSprite;
+var train:FunkinSprite;
+var tunnelBG2:FunkinSprite;
+
+var options:FunkinSprite;
+var freeplay:FunkinSprite;
+var extra:FunkinSprite;
+
+var trainHB:FlxSprite;
+var optionsHB:FlxSprite;
+var freeplayHB:FlxSprite;
+var extraHB:FlxSprite;
+
+import funkin.options.OptionsMenu;
 function create(){
+		FlxG.mouse.visible = true;
 		tunnelBG = new FlxSprite().loadGraphic(Paths.image("f/newMain/subway_bg_2"));
 		add(tunnelBG);
 
-		train = new FlxSprite(250,150);
-		train.scale.set(1.32,1.32);
+		train = new FunkinSprite(220,90);
+		train.scale.set(1.25,1.25);
 		train.frames = Paths.getFrames("f/newMain/trainmenu");
-		add(train);
-		// train.screenCenter();
 		train.animation.addByPrefix('come', 'Train come', 24, false);
 		train.animation.addByPrefix('nsel', 'Train notselected', 24, false);
 		train.animation.addByPrefix('sel', 'Train selected', 24, false);
-		train.animation.play("come");
+		train.addOffset("sel", 0, 31);
+		train.addOffset("conf", 0, 12);
+		train.animation.addByPrefix('conf', 'Train confirm', 24, false);
+		train.playAnim("come");
+		add(train);
 
 		tunnelBG2 = new FlxSprite().loadGraphic(Paths.image("f/newMain/subway_bg"));
 		add(tunnelBG2);
 
+
+		options = new FunkinSprite(915, 560);
+		options.frames = Paths.getFrames("f/newMain/options");
+
+		options.animation.addByPrefix('conf', 'options confirm', 24, false);
+		options.animation.addByPrefix('nsel', 'options notselected', 24, false);
+		options.animation.addByPrefix('sel', 'options selected', 24, false);
+		options.addOffset("nsel", 0, -34);
+
+		options.playAnim("nsel");
+
+		add(options);
+
+
+		freeplay = new FunkinSprite(1110, 160);
+		
+		freeplay.frames = Paths.getFrames("f/newMain/freeplay");
+
+		freeplay.animation.addByPrefix('conf', 'freeplay confirm', 24, false);
+		freeplay.animation.addByPrefix('nsel', 'freeplay notselected', 24, false);
+		freeplay.animation.addByPrefix('sel', 'freeplay selected', 24, false);
+		// freeplay.addOffset("nsel", -256, -170);
+
+		freeplay.playAnim("nsel");
+
+		// freeplay.updateHitbox();
+		freeplay.addOffset("sel", 255, 170);
+		freeplay.addOffset("conf", 255, 170);
+
+		add(freeplay);
+
+
+		extras = new FunkinSprite(840,200);
+		extras.frames = Paths.getFrames("f/newMain/extra");
+		add(extras);
+
+		extras.animation.addByPrefix('conf', 'extras confirm', 24, false);
+		extras.animation.addByPrefix('nsel', 'extras notselected', 24, false);
+		extras.animation.addByPrefix('sel', 'extras selected', 24, false);
+		extras.addOffset("sel", 258,257);
+
+		
+
+
+		extraHB = new FlxSprite(835,200).makeGraphic(160,200);
+		add(extraHB);
+		extraHB.visible = false;
+		trainHB = new FlxSprite(200, 100).makeGraphic(700,400);
+		add(trainHB);	
+		// trainHB.alpha = 0.6;
+		trainHB.visible = false;
+
+		freeplayHB = new FlxSprite(1100, 155).makeGraphic(500,230);
+		add(freeplayHB);	
+		freeplayHB.visible = false;
+		optionsHB = new FlxSprite(900, 550).makeGraphic(400,200);
+		add(optionsHB);	
+		optionsHB.visible = false;
+		
+		for(o in [tunnelBG, tunnelBG2, train, options, freeplay]){
+			o.antialiasing = true;
+			o.updateHitbox();
+		}
+		
+
 }
+var confirmed = false;
 
 function update(){
 	if(controls.BACK){
@@ -27,5 +107,69 @@ function update(){
 	if(controls.RESET){
 		FlxG.switchState(new MainMenuState());
 
+	}
+	if(FlxG.mouse.overlaps(trainHB)){
+		if(train.getAnimName() != "sel" && confirmed == false){
+			train.playAnim("sel", false);
+		}
+		if(FlxG.mouse.justPressed){
+			confirmed = true;
+			train.playAnim("conf",false);
+			new FlxTimer().start(1, function(u:FlxTimer){
+				FlxG.switchState(new StoryMenuState());
+			});
+		}
+	}
+	else {
+		train.playAnim("nsel", false);
+	}
+
+	if(FlxG.mouse.overlaps(optionsHB)){
+		if(options.getAnimName() != "sel" && confirmed == false){
+			options.playAnim("sel", false);
+		}
+		if(FlxG.mouse.justPressed){
+			confirmed = true;
+			options.playAnim("conf",false);
+			new FlxTimer().start(1, function(u:FlxTimer){
+				FlxG.switchState(new OptionsMenu());
+			});
+		}
+	}
+	else {
+		options.playAnim("nsel", false);
+	}
+
+
+
+	if(FlxG.mouse.overlaps(freeplayHB)){
+		if(freeplay.getAnimName() != "sel" && confirmed == false){
+			freeplay.playAnim("sel", false);
+		}
+		if(FlxG.mouse.justPressed){
+			confirmed = true;
+			freeplay.playAnim("conf",false);
+			new FlxTimer().start(1, function(u:FlxTimer){
+				FlxG.switchState(new FreeplayState());
+			});
+		}
+	}
+	else {
+		freeplay.playAnim("nsel", false);
+	}
+	
+	if(FlxG.mouse.overlaps(extraHB)){
+		if(extras.getAnimName() != "sel" && confirmed == false){
+			extras.playAnim("sel", false);
+		}
+		if(FlxG.mouse.justPressed){
+			confirmed = true;
+			extras.playAnim("conf",false);
+			new FlxTimer().start(1, function(u:FlxTimer){
+			});
+		}
+	}
+	else {
+		extras.playAnim("nsel", false);
 	}
 }
